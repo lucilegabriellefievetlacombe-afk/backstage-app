89aa8d5d86fb6eff4609a89ad581856f8b330f4c
Lucile Fievet <lfievet@quad.fr>
2025-12-13 17:11:40 +0100

    shared file

--- backstage/packages/app/src/App.tsx
+++ backstage/packages/app/src/App.tsx
@@ -13,6 +13,7 @@ import { ScaffolderPage, scaffolderPlugin } from '@backstage/plugin-scaffolder';
 import { orgPlugin } from '@backstage/plugin-org';
 import { SearchPage } from '@backstage/plugin-search';
 import {
+  DefaultTechDocsHome,
   TechDocsIndexPage,
   techdocsPlugin,
   TechDocsReaderPage,
@@ -83,7 +84,9 @@ const routes = (
     >
       {entityPage}
     </Route>
-    <Route path="/docs" element={<TechDocsIndexPage />} />
+    <Route path="/docs" element={<TechDocsIndexPage />}>
+    	<DefaultTechDocsHome />
+    </Route>
     <Route
       path="/docs/:namespace/:kind/:name/*"
       element={<TechDocsReaderPage />}
