0e669a6f1d9cce0db009d735db9c0b17ab1f3523
Lucile Fievet <lfievet@quad.fr>
2025-12-19 13:31:25 +0100

    script to configure default source and configurations

---
+++ 1-build-config_bckstg_image-container.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -f .env ]; then 
+	if [ -d backstage ]; then
+
+		# Build alpine-backstage-conf:0.0.0 image
+		docker build -t alpine-backstage-conf:0.0.0. -f Dockerfile-config .
+
+		# Run configuration container
+		# must be in the directory where the backstage source directory have been created
+		# it must contain .env with auth and url for curl configuration
+		# it mounts /app with our current local directory
+		docker run --rm -it -v `pwd`:/app -w /app alpine-backstage-conf:0.0.0
+	else
+		echo 'backstage source and config directory is missing'
+	fi
+else
+	echo '.env with configuration is missing'
+fi
